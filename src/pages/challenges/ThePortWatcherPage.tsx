52d93aa2dee941946b8d3b96f9e444ac7b492cda
SeanKervick <20104300@mail.wit.ie>
2025-03-16 09:24:13 +0000

    port watcher update - commands

--- src/pages/challenges/ThePortWatcherPage.tsx
+++ src/pages/challenges/ThePortWatcherPage.tsx
@@ -1,31 +1,46 @@
-import { Card, CardContent, Box, Input } from "@mui/material";
+import { Card, CardContent, Box, Typography, Input } from "@mui/material";
 import { useState } from "react";
-
+import storedCommands from "../../components/NmapCommands.tsx";
 
 const PortWatcher = () => {
-  const [command, setCommand] = useState<string>("");
+  const [inputCommand, setInputCommand] = useState<string>("");
+  const [output, setOutput] = useState<string>("");
 
+  // https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/forms_and_events/
+  const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      const response = storedCommands[inputCommand]; // look up the response for the entered command
+      setOutput(response ? response : "Invalid command. Try 'nmap -h' for help.");
+      setInputCommand(""); // clear the input field
+    }
+  };
 
   return (
     <Card sx={{ 
       color: "white", 
       width: "90vw", 
-      height: "30vw", 
+      height: "32vw", 
       maxWidth: "900px",
-      margin: "auto",
-      border: "1px solid grey" }}>
+      border: "1px solid grey",
+      overflow: "auto", }}
+      >
       <CardContent>
-        <Box sx={{ fontFamily: "monospace", textAlign: "left"}}>
+        <Box sx={{ fontFamily: "monospace", textAlign: "left", whiteSpace: "pre-line"}}>
+          Command Prompt<br></br>
+          <br></br>
           Microsoft Windows [Version 10.0.26100.3476]<br></br>
           (c) Microsoft Corporation. All rights reserved.<br></br>
           <br></br>
           C:\Users\Player&gt;
         <Input
-          value={command}
-          onChange={(e) => setCommand(e.target.value)}
+          autoFocus 
+          value={inputCommand} // for clearing the input field
+          onChange={(e) => setInputCommand(e.target.value)} // so the input is visible
+          onKeyDown={handleKeyPress} // https://react.dev/reference/react-dom/components/common#handling-keyboard-events
           disableUnderline
           sx = {{ color: "white", fontFamily: "monospace" }}
           />
+          <Typography sx={{ fontFamily: "monospace", mt: 2 }}>{output}</Typography>
         </Box>
       </CardContent>
     </Card>
